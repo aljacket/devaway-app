871af97fe105564fe32317d50648e904c72b043b
a.cavalieri <a.cavalieri@summonpress.com>
2021-08-13 16:08:24 +0200

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1 class="w-full py-4 text-center font-bold">Kart Table</h1>
+    <Table />
+</template>
+
+<script>
+    import Table from '@/components/Table.vue';
+
+    export default {
+        components: {
+            Table
+        }
+    }
+</script>
